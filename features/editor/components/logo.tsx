bce4065171fbbd909ad0dc4194b8be8985a09845
CodeResin <coderesin@gmail.com>
2026-04-09 19:06:17 +0800

    feat:新增导航栏功能 1.封装toolTip组件 2.搭建redo/undo/save/export/file模块 3.具体功能待TODO

--- features/editor/components/logo.tsx
+++ features/editor/components/logo.tsx
@@ -1,1 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="size-8 relative shrink-0">
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          fill
+          className="hover:opacity-75 transition"
+        />
+      </div>
+    </Link>
+  );
+};
--- features/editor/components/navbar.tsx
+++ features/editor/components/navbar.tsx
@@ -1,9 +1,152 @@
 "use client";
+import { CiFileOn } from "react-icons/ci";
+import {
+  ChevronDown,
+  Download,
+  MousePointerClick,
+  Redo2,
+  Undo2,
+} from "lucide-react";
+import { Hint } from "@/components/hint";
+import { Logo } from "./logo";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
+import { BsCloudCheck } from "react-icons/bs";
 
 export const Navbar = () => {
   return (
-    <nav className="w-full flex items-center p-4 h-[68px] gap-x-8 border-b lg:pl-[34px]">
-      navbar
+    <nav className="w-full flex items-center p-4 h-17 gap-x-8 border-b lg:pl-8.5">
+      <Logo />
+      <div className="w-full h-full flex items-center gap-x-1">
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger asChild>
+            <Button size="sm" variant="ghost">
+              File
+              <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start" className="min-w-60">
+            <DropdownMenuItem
+              onClick={() => {
+                console.log("wait to do");
+              }}
+              className="flex items-center gap-x-2"
+            >
+              <CiFileOn className="size-8" />
+              <div>
+                <p>Open</p>
+                <p className="text-xs text-muted-foreground">
+                  Open a JSON file
+                </p>
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Separator orientation="vertical" className="mx-2" />
+        <Hint label="Select" side="bottom" sideOffset={10}>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => {}} //TODO
+            className=""
+          >
+            <MousePointerClick className="size-4" />
+          </Button>
+        </Hint>
+        <Hint label="Undo" side="bottom" sideOffset={10}>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => {}} //TODO
+            className=""
+          >
+            <Undo2 className="size-4" />
+          </Button>
+        </Hint>
+        <Hint label="Redo" side="bottom" sideOffset={10}>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => {}} //TODO
+            className=""
+          >
+            <Redo2 className="size-4" />
+          </Button>
+        </Hint>
+        <Separator orientation="vertical" className="mx-2" />
+        <div className="flex items-center gap-x-2">
+          <BsCloudCheck className="size-4 text-muted-foreground" />
+          <div className="text-xs text-muted-foreground">Saved</div>
+        </div>
+        <div className="flex items-center gap-x-4 ml-auto">
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+              <Button size="sm" variant="ghost">
+                Export
+                <Download className="size-4 ml-2" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="min-w-60">
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}}
+              >
+                <CiFileOn className="size-8" />
+                <div>
+                  <p>JSON</p>
+                  <p className="text-xs text-muted-foreground">
+                    Save for later editing
+                  </p>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}}
+              >
+                {/* TODO */}
+                <CiFileOn className="size-8" />
+                <div>
+                  <p>PNG</p>
+                  <p className="text-xs text-muted-foreground">
+                    Save for later editing
+                  </p>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}}
+              >
+                <CiFileOn className="size-8" />
+                <div>
+                  <p>JPG</p>
+                  <p className="text-xs text-muted-foreground">
+                    Save for later editing
+                  </p>
+                </div>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="flex items-center gap-x-2"
+                onClick={() => {}}
+              >
+                <CiFileOn className="size-8" />
+                <div>
+                  <p>SVG</p>
+                  <p className="text-xs text-muted-foreground">
+                    Save for later editing
+                  </p>
+                </div>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          {/* TODO USER */}
+        </div>
+      </div>
     </nav>
   );
 };
